c4699f34591f8de80f029870636fd6b48833338a
sheva <ivandapratama2@gmail.com>
2023-12-14 17:42:00 +0700

    floatbutton

--- app/src/main/java/com/example/sqlitefixnocap/HomeActivity.kt
+++ app/src/main/java/com/example/sqlitefixnocap/HomeActivity.kt
@@ -16,6 +16,7 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var textnamawarung: EditText
     private lateinit var textlogo: EditText
     private lateinit var btnUploadImage: Button // Changed from EditText to Button
+    private lateinit var btnUploadLogo: Button // Changed from EditText to Button
     private lateinit var tambahkan: Button
     private lateinit var lihatdata: Button
     private lateinit var DB: DBHelper
@@ -29,7 +30,7 @@ class HomeActivity : AppCompatActivity() {
 
         textidwarung = findViewById(R.id.edIdWarung)
         textnamawarung = findViewById(R.id.edNamaWarung)
-        textlogo = findViewById(R.id.logoWarung)
+        btnUploadLogo = findViewById(R.id.btnUploadLogo)
         btnUploadImage = findViewById(R.id.btnUploadImage) // Changed from EditText to Button
         tambahkan = findViewById(R.id.btnAdd)
         lihatdata = findViewById(R.id.btnView)
@@ -39,15 +40,19 @@ class HomeActivity : AppCompatActivity() {
             openGallery()
         }
 
+        btnUploadLogo.setOnClickListener {
+            openGallery()
+        }
+
         tambahkan.setOnClickListener {
             val idwarung = textidwarung.text.toString()
             val namawarung = textnamawarung.text.toString()
-            val logowarung = textlogo.text.toString()
 
-            if (idwarung.isEmpty() || namawarung.isEmpty() || logowarung.isEmpty() || selectedImageUri == null) {
+            if (idwarung.isEmpty() || namawarung.isEmpty() || selectedLogoUri == null || selectedImageUri == null) {
                 Toast.makeText(this@HomeActivity, "Please enter all the fields", Toast.LENGTH_SHORT).show()
             } else {
                 val gambarwarung = selectedImageUri.toString() // Use the image URI
+                val logowarung = selectedLogoUri.toString() // Use the image URI
                 val insert = DB.insertWarung(idwarung, namawarung, logowarung, gambarwarung)
 
                 if (insert) {
@@ -67,6 +72,7 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private var selectedImageUri: Uri? = null
+    private var selectedLogoUri: Uri? = null
 
     private fun openGallery() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
@@ -78,6 +84,7 @@ class HomeActivity : AppCompatActivity() {
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
             selectedImageUri = data.data
+            selectedLogoUri = data.data
             // Optionally, display the selected image's path or name in a TextView or EditText
             // Example: textgambar.setText(selectedImageUri.toString())
         }
