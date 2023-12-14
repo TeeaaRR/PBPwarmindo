a2c30ffa4a741ac9ce98fb257b3b159a4a2a4f71
splendiddolpin <kikibagaskara12@gmail,com>
2023-12-14 14:29:41 +0700

    kintil

--- app/src/main/java/com/example/sqlitefixnocap/CreateMenu.kt
+++ app/src/main/java/com/example/sqlitefixnocap/CreateMenu.kt
@@ -21,14 +21,14 @@ class CreateMenu : AppCompatActivity() {
         val textgambar = findViewById<EditText>(R.id.gambarMenu)
         val textharga = findViewById<EditText>(R.id.hargaMenu)
         val tambahkan = findViewById<Button>(R.id.btnAddMenu)
-        val textkategori = findViewById<EditText>(R.id.kategoriMenu)
+        val textkategori = findViewById<Spinner>(R.id.kategoriMenu)
 
         tambahkan.setOnClickListener {
             val idmenu = textidmenu.text.toString()
             val namamenu = textnamamenu.text.toString()
             val hargamenu = textharga.text.toString()
             val gambarmenu = textgambar.text.toString()
-            val kategorimenu = textkategori.text.toString()
+            val kategorimenu = textkategori.selectedItem.toString()
 
             if (idmenu.isEmpty() || namamenu.isEmpty() || hargamenu.isEmpty() || gambarmenu.isEmpty() || kategorimenu.isEmpty()) {
                 Toast.makeText(this@CreateMenu, "Please enter all the fields", Toast.LENGTH_SHORT).show()
@@ -36,7 +36,7 @@ class CreateMenu : AppCompatActivity() {
                 val insert = DB.insertMenu(idmenu, namamenu, hargamenu, gambarmenu, kategorimenu)
                 if (insert) {
                     Toast.makeText(this@CreateMenu, "Create Menu successfully", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(applicationContext, ViewActivity::class.java)
+                    val intent = Intent(applicationContext, ViewMenu::class.java)
                     startActivity(intent)
                 } else {
                     Toast.makeText(this@CreateMenu, "Create Menu failed", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/sqlitefixnocap/EditMenu.kt
+++ app/src/main/java/com/example/sqlitefixnocap/EditMenu.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 
 class EditMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +18,8 @@ class EditMenu : AppCompatActivity() {
         val namaMenu: String? = intent.getStringExtra("MENU_NAMA")
         val hargaMenu: String? = intent.getStringExtra("MENU_LOGO")
         val gambarMenu: String? = intent.getStringExtra("MENU_GAMBAR")
-        val kategoriMenu: String? = intent.getStringExtra("MENU_GAMBAR")
+        val kategoriMenu: String? = intent.getStringExtra("MENU_KATEGORI")
+        val kategoriArray = resources.getStringArray(R.array.options_array)
 
         // Gunakan nilai-nilai ini untuk menampilkan data pada form edit
         // Misalnya, jika Anda memiliki EditText dengan ID etNamaWarung di layout Anda:
@@ -27,15 +29,20 @@ class EditMenu : AppCompatActivity() {
         etHargaMenu.setText(hargaMenu)
         val etGambarMenu: EditText = findViewById(R.id.etGambarMenu)
         etGambarMenu.setText(gambarMenu)
-        val etKategoriMenu: EditText = findViewById(R.id.etKategoriMenu)
-        etKategoriMenu.setText(kategoriMenu)
+        val etKategoriMenu: Spinner = findViewById(R.id.etKategoriMenu)
+        kategoriMenu?.let {
+            val posisiItem = kategoriArray.indexOf(it)
+            if (posisiItem != -1) {
+                etKategoriMenu.setSelection(posisiItem)
+            }
+        }
         // Lakukan hal yang sama untuk komponen lainnya di dalam form edit
 
         btnSaveEdit.setOnClickListener{
             val editedNama = etNamaMenu.text.toString()
             val editedHarga = etHargaMenu.text.toString()
             val editedGambar = etGambarMenu.text.toString()
-            val editedKategori = etKategoriMenu.text.toString()
+            val editedKategori = etKategoriMenu.selectedItem.toString()
             // Ambil nilai-nilai lainnya dari komponen-komponen di form edit
 
             // Memanggil fungsi updateWarung pada DBHelper
